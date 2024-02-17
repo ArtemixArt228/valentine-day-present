bd6bd35a6785c4a1034a7f93de2d6be083583033
Artemix <artemix926@gmail.com>
2024-02-17 13:26:25 +0200

    feat: Done artemix valentine present

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+function ValentineCard() {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='wrapper'>
+          <div className="container"
+               onMouseEnter={handleMouseEnter}
+               onMouseLeave={handleMouseLeave}
+          >
+              <div className={`valentines ${isHovered ? 'hovered' : ''}`}>
+                  <div className="envelope"></div>
+                  <div className="front"></div>
+                  <div className={`card ${isHovered ? 'up' : ''}`}>
+                      <div className="text">Happy <br />Valentine's <br />Day!</div>
+                      <div className="heart"></div> {/* Note: This div seems to be incomplete in the original HTML */}
+                  </div>
+                  <div className="hearts">
+                      <div className="one"></div>
+                      <div className="two"></div>
+                      <div className="three"></div>
+                      <div className="four"></div>
+                      <div className="five"></div>
+                  </div>
+              </div>
+              <div className="shadow"></div>
+          </div>
+
+          <div className='small-heart-1'>
+              <div className='small-heart'></div>
+          </div>
+          <div className='small-heart-2'>
+              <div className='small-heart'></div>
+          </div>
+          <div className='small-heart-3'>
+              <div className='small-heart'></div>
+          </div>
+          <div className='small-heart-4'>
+              <div className='small-heart'></div>
+          </div>
+      </div>
   );
 }
 
-export default App;
+export default ValentineCard;
